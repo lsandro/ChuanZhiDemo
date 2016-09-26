e57cc83fd120b631709205283bd1794279e5c77f
lsandro <42097314@qq.com>
2016-09-26 19:42:44 +0800

    Initial Commit

---
+++ ChuanZhiDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChuanZhiDemo
+//
+//  Created by YI on 16/9/26.
+//  Copyright © 2016年 Sandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChuanZhiDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChuanZhiDemo
+//
+//  Created by YI on 16/9/26.
+//  Copyright © 2016年 Sandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
